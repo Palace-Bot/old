99c49085272cac7e268a4fbd122976a50a70536b
JiHongYuan <1206429202@qq.com>
2022-03-23 23:22:33 +0800

    feat: qq登录读取yaml配置

--- src/main/java/org/github/palace/bot/MainApplication.java
+++ src/main/java/org/github/palace/bot/MainApplication.java
@@ -5,13 +5,16 @@ import net.mamoe.mirai.BotFactory;
 import org.github.palace.bot.builder.BotConfigurationBuilder;
 import org.github.palace.bot.core.EventDispatcher;
 
+import static org.github.palace.bot.constant.BaseConstant.PASSWORD;
+import static org.github.palace.bot.constant.BaseConstant.QQ;
+
 /**
  * @author JHY
  * @date 2022/3/22 10:38
  */
 public class MainApplication {
     public static void main(String[] args) {
-        Bot bot = BotFactory.INSTANCE.newBot(1515023757, "", BotConfigurationBuilder.builder());
+        Bot bot = BotFactory.INSTANCE.newBot(QQ, PASSWORD, BotConfigurationBuilder.builder());
         bot.login();
 
         EventDispatcher eventDispatcher = new EventDispatcher();
