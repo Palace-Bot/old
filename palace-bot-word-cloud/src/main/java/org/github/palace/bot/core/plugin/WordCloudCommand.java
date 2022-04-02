6dca56408fedd28641c1555124bc7bc726c394f9
JiHongYuan <1206429202@qq.com>
2022-04-02 22:40:15 +0800

    feat: 命令处理器映射 (以后优化

--- palace-bot-word-cloud/src/main/java/org/github/palace/bot/core/plugin/WordCloudCommand.java
+++ palace-bot-word-cloud/src/main/java/org/github/palace/bot/core/plugin/WordCloudCommand.java
@@ -1,8 +1,10 @@
 package org.github.palace.bot.core.plugin;
 
-import net.mamoe.mirai.contact.Group;
-import net.mamoe.mirai.contact.Member;
+import net.mamoe.mirai.message.data.At;
+import net.mamoe.mirai.message.data.MessageSource;
+import net.mamoe.mirai.message.data.PlainText;
 import org.github.palace.bot.core.annotation.CommandHandler;
+import org.github.palace.bot.core.cli.CommandSender;
 import org.github.palace.bot.core.cli.SimpleCommand;
 
 /**
@@ -11,13 +13,12 @@ import org.github.palace.bot.core.cli.SimpleCommand;
  */
 public class WordCloudCommand extends SimpleCommand {
     public WordCloudCommand() {
-        super(null, null, false, null);
-
+        super("查看个人词云", null, false, null);
     }
 
     @CommandHandler
-    public void handler(Member member, Group group) {
-        System.out.println("11111111111");
+    public void handler(CommandSender commandSender, MessageSource messageSource, PlainText plainText) {
+        commandSender.sendMessage(new At(messageSource.getFromId()).plus(" ").plus(plainText));
     }
 
 }
